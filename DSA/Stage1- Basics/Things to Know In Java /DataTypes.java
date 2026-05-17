4a542f4e9d13dd797d39467faddfadbf91048b89
Aryadeep Varshney <152092016+aryadeep02@users.noreply.github.com>
2026-05-17 21:39:50 +0530

    Add dataTypeSize method to determine data type sizes

---
+++ DSA/Stage1- Basics/Things to Know In Java /DataTypes.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int dataTypeSize(String str) {
+        // Check the input string and return the corresponding size in bytes
+    
+        // If the input is "Character", return 2 bytes
+        if (str.equals("Character")) {
+            return 2;
+        } 
+        // If the input is "Integer", return 4 bytes
+        else if (str.equals("Integer")) {
+            return 4;
+        } 
+        // If the input is "Long", return 8 bytes
+        else if (str.equals("Long")) {
+            return 8;
+        } 
+        // If the input is "Float", return 4 bytes
+        else if (str.equals("Float")) {
+            return 4;
+        } 
+        // If the input is "Double", return 8 bytes
+        else if (str.equals("Double")) {
+            return 8;
+        }
+    
+        // Return -1 if the input does not match any known data type
+        return -1;
+    }
