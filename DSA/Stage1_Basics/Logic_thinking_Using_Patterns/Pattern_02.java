b1b2c64323af43ef9ebe44afb85b91611093fcf8
Aryadeep Varshney <aryadeepcool1022@gmail.com>
2026-05-19 00:44:40 +0530

    changes

--- DSA/Stage1_Basics/Logic_thinking_Using_Patterns/Pattern_02.java
+++ DSA/Stage1_Basics/Logic_thinking_Using_Patterns/Pattern_02.java
@@ -3,9 +3,9 @@ package Stage1_Basics.Logic_thinking_Using_Patterns;
 public class Pattern_02 {
     static void main() {
         int start=5;
-        for(int i=1;i<=start;i++){//initialised for i
-            for(int j=1;j<=i;j++){//initialised for j
-                System.out.print(j);//prints in range of j
+        for(int i=1;i<=start;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
             }
             System.out.println();
         }
--- DSA/Stage1_Basics/Logic_thinking_Using_Patterns/Pattern_04.java
+++ DSA/Stage1_Basics/Logic_thinking_Using_Patterns/Pattern_04.java
@@ -3,7 +3,7 @@ package Stage1_Basics.Logic_thinking_Using_Patterns;
 public class Pattern_04 {
     static void main() {
         int total=5;
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<=total;i++){
             for(int j=1;j<=total;j++){
                 System.out.print("*");
             }
